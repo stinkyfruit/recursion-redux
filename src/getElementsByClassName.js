355749e3c514551bd53d9296465c6a4dfc3a01c3
Kristen Haydel <kristen@whatever.com>
2015-11-16 18:11:33 -0600

    Reimplemented getElementsByClassName HR Day1

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className, node){
   // your code here
+  var nodes = [];
+  node = node || document.body;
+  // compare className to node's class
+  
+  var classes = node.className.split(' ');
+  if(classes.indexOf(className) >= 0) {
+  //save the nodes
+    nodes.push(node);
+  } 
+  // iterate over the node's children
+  for(var i = 0; i < node.children.length; i++) {
+    if(node.children[i]) {
+
+      nodes = nodes.concat(getElementsByClassName(className, node.children[i]));
+    }
+  }
+    // save the child nodes
+  return nodes;
 };
+
+
