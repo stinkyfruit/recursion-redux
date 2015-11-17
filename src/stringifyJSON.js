70d79eaef3767fddc46ec6eafeb5534f7e701bfb
Kristen Haydel <kristen@whatever.com>
2015-11-17 10:52:56 -0600

    Reimplemented stringifyJSON - Day2 HR

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,35 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if(Array.isArray(obj)){
+    if (obj.length === 0) {
+      return '['+obj+']';
+    }
+    var results = [];
+    for (var i = 0; i < obj.length; i++) {
+      results.push(stringifyJSON(obj[i]));
+    }
+    return '['+results.join(',')+']';
+  }
+
+  if(typeof obj === 'object' && obj){
+  
+    var pieces = [];
+    for( var key in obj ) {
+      if (typeof (obj[key]) === 'function' || obj[key] === undefined){
+        continue;
+      }
+      pieces.push( stringifyJSON(key) +':'+ stringifyJSON(obj[key]));
+    }
+    return '{'+pieces.join(',')+'}'; 
+  }
+//expected '{"foo":true}' to equal '{"foo":true,"bar":false,"baz":null}'
+
+  if(typeof obj === 'string'){
+    return '"'+obj+'"';
+  }
+  return ''+obj;
 };
+
+// expected '[8' to equal '[8]'
